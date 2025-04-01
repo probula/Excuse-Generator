26bf7975f79600ea6bc1105911ed3da2dd36848d
Igor <igor.kowalczyk@cyfroweszkoly.pl>
2025-04-01 13:24:36 +0200

    added useState for form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+    const [name, setName] = useState('');
+    const [reason, setReason] = useState('wybierz');
+    const [reliability, setReliability] = useState(5);
+    const [date, setDate] = useState("");
+    const [creativity, setCreativity] = useState('wybierz');
+    const [details, setDetails] = useState('');
+    const [urgent, setUrgent] = useState(false);
+
+    const excuses = {
+        "spóźnienie": "kosmici porwali mojego psa",
+        "niedostarczenie pracy domowej": "mój chomik zjadł moją pracę",
+        "brak odpowiedzi na wiadomość": "telefon mi eksplodował",
+    };
+
+
+
     return (
         <>
             <form>
                 <label>
                     Podaj imię:
-                    <input type={'text'} />
+                    <input type={'text'} value={name} onChange={(e) => setName(e.target.value)}/>
                 </label>
                 <label>
                     Powód wymówki:
-                    <select>
+                    <select value={reason} onChange={(e) => setReason(e.target.value)}>
                         <option>wybierz</option>
                         <option>spóźnienie</option>
                         <option>niedostarczenie pracy domowej</option>
@@ -19,13 +36,14 @@ function App() {
                 </label>
                 <label>
                     Poziom wiarygodności:
-                    <input type={'range'} min={0} max={10} defaultValue={5} />
+                    <input type={'range'} min={0} max={10} value={reliability}
+                           onChange={(e) => setReliability(Number(e.target.value))} />
                 </label>
                 <label>
-                    Data zdarzenia: <input type={'date'}></input>
+                    Data zdarzenia: <input type={'date'} value={date} onChange={(e) => setDate(e.target.value)}></input>
                 </label>
                 <label>
-                    Poziom kreatywności: <select>
+                    Poziom kreatywności: <select value={creativity} onChange={(e) => setCreativity(e.target.value)}>
                         <option>wybierz</option>
                         <option>duży</option>
                         <option>średni</option>
@@ -33,12 +51,11 @@ function App() {
                     </select>
                 </label>
                 <label>
-                    Dodatkowe szczegóły: <textarea id={"textarea"}>
-
+                    Dodatkowe szczegóły: <textarea id={"textarea"} value={details} onChange={(e) => setDetails(e.target.value)}>
                     </textarea>
                 </label>
                 <label>
-                    Pilność wymówki: <input type={"checkbox"}></input>
+                    Pilność wymówki: <input type={"checkbox"} checked={urgent} onChange={(e) => setUrgent(e.target.checked)}></input>
                 </label>
                 <label>
                     <input type={"button"} value={"Wyślij"}></input>
